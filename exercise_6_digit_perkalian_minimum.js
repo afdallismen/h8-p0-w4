b828d3721effda60ab2b8848f792933bbb1694c4
afdallismen <afdal.lismen@gmail.com>
2019-02-13 13:29:55 +0700

    Finishing exercise 6 and 9

--- exercise_6_digit_perkalian_minimum.js
+++ exercise_6_digit_perkalian_minimum.js
@@ -1,4 +1,24 @@
 function digitPerkalianMinimum(angka) {
+    var leftOperand = []
+    var rightOperand = []
+    var unique = true
+    var i = 1
+    while (unique && i <= angka) {
+        if (angka % i === 0) {
+            var j = 0
+            while (j < rightOperand.length) {
+                unique = i !== rightOperand[j]
+                j++
+            }
+
+            if (unique) {
+                leftOperand.push(i)
+                rightOperand.push(angka / i)
+            }
+        }
+        i++
+    }
+    return ('' + leftOperand.pop() + rightOperand.pop()).length
 }
 
 console.log(digitPerkalianMinimum(24))
--- exercise_9_check_ab.js
+++ exercise_9_check_ab.js
@@ -1,4 +1,21 @@
 function checkAB(num) {
+    var indexA = undefined
+    var i = 0
+    while (indexA === undefined && i < num.length) {
+        indexA = num[i] === 'a' ? i : undefined 
+        i++
+    }
+    if (indexA === undefined) {
+        return false
+    } else {
+        var found = false
+        var j = indexA + 1
+        while (!found && j < (indexA + 5)) {
+            found = num[j] === 'b'
+            j++
+        }
+        return found
+    }
 }
 
 console.log(checkAB('lane borrowed'))
